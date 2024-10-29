54828ba5d47397d482e31dcacfe82253a5be6c9f
Kosheew <abdrey192020@gmail.com>
2024-10-29 11:19:19 +0200

    Data info Capitol

--- Assets/Scripts/StateBuild/UpgradeResource.cs
+++ Assets/Scripts/StateBuild/UpgradeResource.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Build", menuName = "ScriptableObjects/Resource Upgrade")]
 public class UpgradeResource : ScriptableObject
 {
+    [Header("Time")]
     [SerializeField] private double _temeBuild;
+
+    [Header("Paymant")]
     [SerializeField] private int _coins;
     [SerializeField] private int _bonds;
+
+    [Header("Resources")]
     [SerializeField] private int _oil;
     [SerializeField] private int _coal;
     [SerializeField] private int _ore;
     [SerializeField] private int _wood;
 
+    [Header("Income")]
+    [SerializeField] private int _income;
     public double TimeBuild => _temeBuild;
     public int Coins => _coins;
     public int Bonds => _bonds;
@@ -18,4 +25,5 @@ public class UpgradeResource : ScriptableObject
     public int Coal => _coal;
     public int Ore => _ore;
     public int Wood => _wood;
+    public int Income => _income;
 }
